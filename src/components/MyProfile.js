4d078367f25d3b78a814893164027bbdb1e4bd18
github username <emilyn0614@gmail.com>
2021-05-31 02:52:29 -0500

    testing git

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+
 const MyProfile = (props) => {
   const testing = useSelector(state => state.usersState.current_user)
   const myAccount = useSelector(state => state.usersState.current_user.user)
