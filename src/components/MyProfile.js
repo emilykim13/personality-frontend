e20a528698ae430512fb610ad44a42fe7dc41311
github username <github email address>
2021-05-30 19:20:07 -0500

    user can now create and update profile without errors

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -32,7 +32,8 @@ console.log(testing.profiles.length)
   const myTests = (myRawTests === "emers") ? console.log("note: no tests taken yet -myTests") : myRawTests.filter(test => test.results !== null) 
   const lastIndex = (myRawTests === "emers") ? console.log("note: no tests taken yet -lastIndex") : myTests.length - 1 
   const prsnts = useSelector(state => state.personalitiesState.personalities)
-  const filteredPersonality = (myRawTests === "emers") ? console.log("note: no tests taken yet -filterPersonality") :  prsnts.filter(p => p.letters === myTests[lastIndex].results)[0]
+  const filteredPersonality = (myRawTests === "emers") ? console.log("note: no tests taken yet -filterPersonality") : prsnts.filter(p => p.letters === myTests[lastIndex].results)[0]
+  console.log(prsnts)
   const linkyloo = (myRawTests === "emers") ? console.log("cannot get filteredPersonality - no test created yet") : `/${filteredPersonality.name.toLowerCase()}`
 
 
@@ -59,11 +60,13 @@ console.log(testing.profiles.length)
 {      return (
           <div className="App">
             <h1>Ni haody, {myAccount.name}!</h1>
+            <img className="p-avatar" src={currentProfile.photo}></img><br/><br/>
             <p>Username: {myAccount.name}</p>
-            <p>{myAccount.email}</p>
-            <p>{currentProfile.username}</p>
-            <h1> You have not taken the WhoRU Personality Test yet, please take the test to view your results here! </h1>
+            <p>Email: {myAccount.email}</p>
+            <p>Full Name: {currentProfile.first_name} {currentProfile.last_name}</p>
             <Link to="/editAccount" className="menu-btn">Edit Account</Link>
+            <h1> You have not taken the WhoRU Personality Test yet, please take the test to view your results here! </h1>
+            <Link to="/loadTest" className="menu-btn">Take a placement test!</Link>
           </div>
         )}
 // else is working good. conditions that are not met, lead to else returned
@@ -71,14 +74,18 @@ console.log(testing.profiles.length)
 {      return (
         <div className="App">
           <h1>Ni haody, {myAccount.name}!</h1>
+          <img className="p-avatar" src={currentProfile.photo}></img><br/><br/>
           <p>Username: {myAccount.name}</p>
-          <p>{myAccount.email}</p>
+          <p>Email: {myAccount.email}</p>
+          <p>Full Name: {currentProfile.first_name} {currentProfile.last_name}</p>
+          <Link to="/editAccount" className="menu-btn">Edit Account</Link>
           <h1> Your latest personality type: {myTests[lastIndex].results}</h1>
           <img onClick={() => props.history.push(linkyloo)} className="p-avatar" src={filteredPersonality.image}></img>
           <p>{filteredPersonality.introduction}</p>
           <p>{filteredPersonality.intropp}</p>
           <h1> You can read more about <Link to={linkyloo}>{filteredPersonality.name}s</Link> and other personality types <Link to="/personalities">here</Link>.</h1>
-          <Link to="/editAccount" className="menu-btn">Edit Account</Link>
+          <Link to="/loadTest" className="menu-btn">Retake Test</Link>
+
         </div>
       )}
 
